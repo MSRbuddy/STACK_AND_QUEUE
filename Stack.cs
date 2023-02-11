66ace6cf7f0c09d06105f25546a296b4dc13a36c
MSRbuddy <meghanadasari222@gmail.com>
2023-02-11 20:15:32 +0530

    Welcome to Stack Problem!

---
+++ Stack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackProgram
+{
+    internal class Stack
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to Stack Program! *****");
+        }
+    }
+}
